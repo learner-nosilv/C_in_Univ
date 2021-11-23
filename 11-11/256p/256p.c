aae0ceeaf13d82d52488ca3e22d5a6e4a68df6bb
nosilv <nosilv8@gmail.com>
2021-11-23 11:18:56 +0900

    I'm in JEJU

--- 11-11/256p/256p.c
+++ 11-11/256p/256p.c
@@ -3,23 +3,17 @@
 
 int main(void)
 {
-	printf("문자를 입력하시오: ");
-	int str = getchar();
+	do {
+		printf("문자를 입력하시오: ");
+		char str = getchar();
+	}while(65str)
 
 	switch (str) {		// 알파벳 자모 구분에 관한 자세한 것은 덮어두는 걸로
-		case ('A'):
-		case ('E'):
-		case ('I'):
-		case ('O'):
-		case ('U'):
-		case ('a'):
-		case ('e'):
-		case ('i'):
-		case ('o'):
-		case ('u'):
-			printf("모음입니다\n");
+		case ('A'): case ('E'): case ('I'): case ('O'): case ('U'):
+		case ('a'): case ('e'): case ('i'): case ('o'): case ('u'):
+			printf("모음입니다\n");	break;
 		default:
-			printf("자음입니다\n");
+			printf("자음입니다\n");	break;
 	}
 	return 0;
 }
