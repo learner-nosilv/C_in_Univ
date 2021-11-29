4a34aca91e0307b53cbe8e4e6b5183c20bb10338
nosilv <nosilv8@gmail.com>
2021-11-29 17:06:51 +0900

    ~ 288p example #3

--- 11-29/285p/285p.c
+++ 11-29/285p/285p.c
@@ -15,5 +15,17 @@ int main(void) {
 	printf("1~10 정수 합 : %d\n", sum);
 	printf("TEST: %d\n", 10 * 11 / 2);
 
+	// cubing.c 세제곱= cubing
+	
+	printf("1부터 정수 '몇'까지의 세제곱값을 보고 싶나요? ");
+	int n;
+	scanf("%d", &n);
+	printf("===================\n");
+	printf("   i	i의 세제곱\n");
+	printf("===================\n");
+
+	for (i = 1; i <= n; i++) printf("%4d	%5d\n", i, i * i * i);
+	
+	//
 	return 0;
 }
