f47d0418539d1ea995537e89c3a63b3298a90cda
nosilv <nosilv8@gmail.com>
2021-12-16 13:45:04 +0900

    2021-12-16(Thu)

--- 310p-/312p/312p.c
+++ 310p-/312p/312p.c
@@ -1,7 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main(void) {
-	for (; ; ) { printf("2"); }
+int main() {
+	
+	int i;
+	for (i = 10; i >= 0; i--) {
+		printf("%d\n", i);
+	}
+
+	int x = 10;
+	while (x >= 0) {
+		printf("%d\n", x);
+		x--;
+	}
+	
 	return 0;
 }
