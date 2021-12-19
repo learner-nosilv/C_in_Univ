4637a6d45db51e6c1270e06b331abc7911a1d8c4
nosilv <nosilv8@gmail.com>
2021-12-19 15:06:28 +0900

    21-12-18(Sat)

---
+++ ch7_programming/08/8.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int length;
+	while (1) {
+		printf("막대의 길이(종료: -1): ");
+		scanf("%d", &length);
+		if (length == -1) break;
+		else if (length < 1 || length>50) printf("1~50사이의 숫자를 입력부탁드립니다.\n");
+		else {
+			for (int i = 0; i < length; i++) printf("*");
+			printf("\n");
+		}
+	}
+	return 0;
+}
