6c161e5f72035156e5eee8e369aa01edffd7c354
nosilv <nosilv8@gmail.com>
2021-12-23 15:39:30 +0900

    2021-12-22(Wed)

--- Chapter_08/336p/is_prime.c
+++ Chapter_08/336p/is_prime.c
@@ -5,25 +5,22 @@ int is_prime(int num);
 
 int main(void) {
 	int num;
-	printf("Is it 'Prime'? _\b");
+	printf("[ Is it 'Prime'? ]\n");
+	printf("Range: 1 ~ _\b");
 	scanf("%d", &num);
+	printf("Prime list: ");
 
-	switch (is_prime(num))
-	{
-		case 0:
-			printf("No, It isn't.\n\n");
-		case 1:
-			printf("Yes, It's prime.\n\n");
-		default:
-			break;
+	for (int n = 2; n <= num; n++) {
+		if (is_prime(n))
+			printf("%d ", n);
 	}
-
+	printf("- END\n");
 	return 0;
 }
 
 int is_prime(int num) {
-	for (int i = 1; i < num; i++) {
+//	if (num == 1) return 0;			// 소수 정의 1) 1보다 큰 자연수
+	for (int i = 2; i < num; i++)	// 소수 정의 2) 1과 자기자신으로'만' 나눠지는 자연수
 		if (num % i == 0) return 0;
-	}
 	return 1;
 }
